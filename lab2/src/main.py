1c9346b7a18a86a9b664b45502a2d62ada575d25
Mariya Senina <senina.m.m@gmail.com>
2022-02-28 12:43:31 +0300

    Newton method done

--- lab2/src/main.py
+++ lab2/src/main.py
@@ -1,5 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.plot([1,2 ,3,4])
-plt.show()
+from input_data import read_equation_console
+from input_data import read_interval_console
+from newton_method import find_root_newton
+
+epsilon = 0.0001
+
+equation = read_equation_console()
+start, stop = read_interval_console()
+
+root = find_root_newton(equation, start, stop, epsilon)
+if root == None: print("Invalide interval and equation for Newton alculation method")
+else:
+    plt.plot([i for i in np.arange(start, stop, 0.01)], [equation(i) for i in np.arange(start, stop, 0.01)])
+    plt.plot(root, equation(root), 'o')
+    plt.title(f"Newton method. Root in [{start},{stop}]")
+    plt.show()
+
---
+++ lab2/src/newton_method.py
@@ -0,0 +1,39 @@
+import numpy as np
+from scipy.misc import derivative
+from sympy import false, true
+
+def find_root_newton(equation, start, stop, epsilon):
+    if not check_interval(equation, start, stop): return None
+    x0 = choose_x0(equation, start, stop)
+    x1 = x0  - equation(x0) / derivative(equation, x0, n=1)
+
+    xi = x1
+    xi_prev = x0
+    while(abs(xi - xi_prev) > epsilon):
+        xi_prev = xi
+        xi = xi - equation(xi) / derivative(equation, xi, n=1)
+    return xi
+
+
+
+def check_interval(equation, start, stop):
+    #TODO: how to check that function is defined and infinit on interval
+    if  not equation(start)*equation(stop) < 0: return false
+    start_derivative_fst = derivative(equation, start, n=1)
+    start_derivative_snd = derivative(equation, start, n=2)
+
+    # points = [round(x * 0.01, 1) for x in range(start, stop)]
+    for i in np.arange(start, stop, 0.01):
+        if not ((start_derivative_fst*derivative(equation, i, n=1) > 0) 
+        and (start_derivative_snd*derivative(equation, i, n=2) >= 0)):
+            return false
+        else: 
+            return true
+
+
+def choose_x0(equation, start, stop):
+    start_derivative_snd = derivative(equation, start, n=2)
+    if(start_derivative_snd*start > 0):
+        return start
+    else:
+        return true
